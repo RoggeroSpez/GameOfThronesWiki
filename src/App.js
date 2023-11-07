e661258fc6f13cb04a5e85540d2d68d96f948085
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-06 18:49:42 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Characters from './pages/Characters';
-import Houses from './pages/Houses';
-import Home from './pages/Home';
+import Characters from './Pages/Characters';
+import Houses from './Pages/Houses';
+import Home from './Pages/Home';
 import NavBar from './components/NavBar';
-
 import gameOfThronesAudio from './Media/gameofthrones.mp3';
 
 function App() {
@@ -25,7 +24,7 @@ function App() {
         <div className="audio-container">
           <audio controls autoPlay>
             <source src={gameOfThronesAudio} type="audio/mp3" />
-            Your browser does not support the audio element.
+            Lord Tyron has gone to go get the minstrals.
           </audio>
         </div>
       </div>
