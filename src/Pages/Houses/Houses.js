0ce786ef7df205dbf36b6ab4e7985902df0189e7
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-06 19:44:30 -0800

    Update Houses.js

--- src/Pages/Houses/Houses.js
+++ src/Pages/Houses/Houses.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import HouseCard from './HouseCard';
+import HouseCard from './Components/HouseCard'
 
 const Houses = () => {
   const [house, setHouse] = useState([]);
