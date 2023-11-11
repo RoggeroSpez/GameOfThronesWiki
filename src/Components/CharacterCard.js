dff9065d9b81accce453df038b65f9832c437b76
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-11 14:01:54 -0800

    Updates

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -5,19 +5,15 @@ const CharacterCard = ({ character }) => {
   return (
     <div className="character-card">
         <h2>{character.name || character.aliases[0]}</h2>
-        <section className='Primary'>
         <p>Born: {character.born || 'Unknown'}</p>
         <p>Died: {character.died || 'Unknown'}</p>
         <p>Gender: {character.gender || 'Unknown'}</p>
         <p>Father: {character.father || 'Unknown'}</p>
         <p>Mother: {character.mother || 'Unknown'}</p>
         <p>Spouse: {character.spouse || 'Unknown'}</p>
-        </section>
-        <section className='Secondary'>
         <p>Culture: {character.culture}</p>
         <p>Titles: {character.titles || 'Unknown'}</p>
         <p>Aliases: {character.aliases || 'Unknown'}</p>
-        </section>
     </div>
   );
 };
